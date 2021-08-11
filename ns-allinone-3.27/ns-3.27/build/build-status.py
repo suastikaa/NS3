6a9b15dd047343fd68346a1ce6e1d0e2af3473a7
root <soeastika98@gmail.com>
2021-08-11 19:28:47 +0800

    test

---
+++ ns-allinone-3.27/ns-3.27/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/subdir/ns3.27-subdir-debug', 'build/scratch/ns3.27-scratch-simulator-debug', 'build/scratch/ns3.27-TA-debug', 'build/scratch/ns3.27-suas-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
